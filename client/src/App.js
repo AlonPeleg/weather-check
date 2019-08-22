ec1d73e5ba8f926e49cecedf4659f86ab9681669
AlonPeleg <peleg619@gmail.com>
2019-08-22 14:47:56 +0300

    .

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,26 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Weather from "./components/weather/Weather";
 import Navbar from "./components/layout/Navbar";
+import Spinner from "./components/layout/Spinner";
 import "./App.css";
 
 const App = () => {
   const [info, setInfo] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
+    setLoading(true);
     setTimeout(async () => {
       const res = await axios.get("/getInfo");
       setInfo(res.data);
+      setLoading(false);
     }, 2000);
   }, []);
 
   return (
-    <div>
+    <div className="App">
       <Navbar />
       <div className="container">
-        <Weather info={info} />
+        {!loading ? <Weather info={info} /> : <Spinner />}
       </div>
     </div>
   );
